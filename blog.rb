8cd3af1b791355dca1fe7e3fd8e882f4f5ad340b
easonhan <nbkhic@qq.com>
2013-06-05 13:35:00 +0800

    add static html builder

--- blog.rb
+++ blog.rb
@@ -7,19 +7,43 @@ set :bind, '0.0.0.0'
 set :port, 3000
 set :erb, layout_engine: :erb
 posts_dir = File.join('.', 'posts') 
+html_dir = File.join('.', 'public', 'p') 
 
 get '/' do
 	posts = []
-	Dir.new(posts_dir).each do |f|
-		posts.push(f.encode('utf-8').gsub('.md', '')) if f.match(/\.md$/)	
+	Dir.new(html_dir).each do |f|
+		posts.push(f.encode('utf-8').gsub('.html', '')) if f.match(/\.html$/)	
 	end
 
   erb :index, locals: { list: posts }
 end
 
 get '/post/*' do |p|
+	render_md(p, posts_dir)
+end
+
+get '/list' do
+	posts = []
+	Dir.new(posts_dir).each do |f|
+		posts.push(f.encode('utf-8').gsub('.md', '')) if f.match(/\.md$/)	
+	end
+
+	erb :list, locals: { list: posts }
+end
+
+get '/build/*' do |p|
+	request = Rack::MockRequest.new(Sinatra::Application)
+	html = request.get("/post/#{p}").body
+	html_path = File.join(html_dir, "#{p}.html") 
+	File.open(html_path, 'w') do |f|
+		f.write html
+	end
+	redirect to('/')
+end
+
+def render_md(file, posts_dir)
   str = ''
-	File.open File.join(posts_dir, "#{p}.md") do |f|
+	File.open File.join(posts_dir, "#{file}.md") do |f|
     str = f.read
   end
 
@@ -31,5 +55,4 @@ get '/post/*' do |p|
                                      :fenced_code_blocks => true)
   html = markdown.render str
   erb :post, locals: { content: html, layout: true } 
-
 end
